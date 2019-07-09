d1b4f9f1df7e76d3bce4548ac5c5deafcdca55db
BaronKimaru <bizzinali@gmail.com>
2019-07-09 15:36:23 +0300

    initial commit

--- project/posts/routes.py
+++ project/posts/routes.py
@@ -15,9 +15,9 @@ Posts = articles()
 @posts_blueprint.route('/posts')
 def posts():
     """Display All Posts"""
-    return render_template('posts.html', posts = Posts)
+    return render_template('posts/posts.html', posts = Posts)
 
 @posts_blueprint.route('/post/<string:id>')
 def post(id):
     """Display A Specific Post"""
-    return render_template('post.html', id = id)
+    return render_template('posts/post.html', id = id)
