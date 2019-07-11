3f4ef57dcfaaf2df46f9b2c32c4dc0e8780d1b4b
BaronKimaru <bizzinali@gmail.com>
2019-07-11 05:39:06 +0300

    Update __init__.py

--- project/home/__init__.py
+++ project/home/__init__.py
@@ -1,5 +1,10 @@
 from flask import Blueprint
 
-home_blueprint = Blueprint('home', __name__, template_folder='templates')
+home_blueprint = Blueprint('home', __name__, template_folder='templates', static_folder='project/static')
 
 from . import routes
+
+
+# http://flask.pocoo.org/docs/1.0/blueprints/#static-files
+# static_folder='static'
+# url_for('blueprint_name.static', filename='style.css')
