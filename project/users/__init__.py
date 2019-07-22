db8ca54ac775668e2b7ab08dce490ab593202f4c
BaronKimaru <bizzinali@gmail.com>
2019-07-22 14:11:09 +0300

    Update __init__.py

--- project/users/__init__.py
+++ project/users/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-#specify name of blueprint 'posts' as __name__. specify location of templates
+#specify name of blueprint 'users' as __name__. specify location of templates
 users_blueprint = Blueprint('users', __name__, template_folder='templates', static_folder=None)
 
 from . import routes
